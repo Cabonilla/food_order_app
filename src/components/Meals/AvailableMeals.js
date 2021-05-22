fd16a3a63f09d718de73402972aeae0e2bbd0ebc
cabonilla <christian.bonilla01@gmail.com>
2021-05-22 11:29:16 -0700

    Now fetching meals data from Firebase API.

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,41 +1,42 @@
+import { useEffect, useState } from 'react'
 import Card from '../UI/Card'
 import classes from './AvailableMeals.module.css'
 import MealItem from './MealItem/MealItem'
 
-const DUMMY_MEALS = [
-  {
-    id: 'm1',
-    name: 'Udon Noodle',
-    description: 'A single, dense noodle in Miso ramen soup.',
-    price: '5.99',
-  },
-  {
-    id: 'm2',
-    name: 'Rice Noodle',
-    description: 'Delicious rice noodle cooked to perfection.',
-    price: '5.99',
-  },
-  {
-    id: 'm3',
-    name: 'Sweet and Sour Chicken',
-    description: 'Chicken that is both sweet and sour.',
-    price: '7.99',
-  },
-  {
-    id: 'm4',
-    name: 'Barbeque Pork',
-    description: 'Slow cooked pork in red sauce.',
-    price: '8.99',
-  }
-]
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <MealItem 
+  const [meals, setMeals] = useState([])
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-f4daa-default-rtdb.firebaseio.com/meals.json').then()
+      const responseData = await response.json()
+
+      const loadedMeals = []
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price
+        })
+      }
+
+      setMeals(loadedMeals)
+    }
+
+    fetchMeals()
+  }, [])
+
+const mealsList = meals.map((meal) => (
+  <MealItem
+    key={meal.id}
     id={meal.id}
-    key={meal.id} 
-    name={meal.name} 
-    description={meal.description} 
-    price={meal.price}/>)
+    name={meal.name}
+    description={meal.description}
+    price={meal.price}
+  />
+))
 
   return <section className={classes.meals}>
     <Card>
